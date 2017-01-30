266166aa758bf335f2658eafe55a590480323d9a
Frederik  Moller <frederik@keconcepts.ke.co.za>
2017-01-30 14:00:37 +0200

    second commit

--- SpeedMeter/MainWindow.xaml.cs
+++ SpeedMeter/MainWindow.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -32,6 +37,16 @@ namespace SpeedMeter
         private readonly string _registryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
         private readonly string _appName = "SpeedMeterV2";
 
+        private static List<Ping> pingers = new List<Ping>();
+        private static int instances = 0;
+        private static object @lock = new object();
+        private static int ttl = 5;
+        private static int timeOut = 250;
+        private static int result = 0;
+        private List<Computers> _lsActiveIPAddresses = new List<Computers>();
+
+        BackgroundWorker _backgroundWorker = new BackgroundWorker();
+
         #endregion
 
         #region Constructor
@@ -39,6 +54,11 @@ namespace SpeedMeter
         public MainWindow()
         {
             InitializeComponent();
+
+            _backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            _backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+
+            Height = 70;
 
             //Startup Location.
             Left = SystemParameters.PrimaryScreenWidth - Width;
@@ -108,15 +128,17 @@ namespace SpeedMeter
 
         private void mnuRunAtStartup_Checked(object sender, RoutedEventArgs e)
         {
-            RegistryKey rk = Registry.CurrentUser.OpenSubKey(_registryKey, true);
-
-            if (mnuRunAtStartup.IsChecked)
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(_registryKey, true))
             {
-                rk.SetValue(_appName, System.Reflection.Assembly.GetExecutingAssembly().Location);
+                key.SetValue(_appName, System.Reflection.Assembly.GetExecutingAssembly().Location);
             }
-            else
+        }
+        
+        private void mnuRunAtStartup_Unchecked(object sender, RoutedEventArgs e)
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(_registryKey, true))
             {
-                rk.DeleteValue(_appName, false);
+                key.DeleteValue(_appName, false);
             }
         }
 
@@ -132,6 +154,73 @@ namespace SpeedMeter
             }
         }
 
+        private void mnuExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        public void Ping_completed(object s, PingCompletedEventArgs e)
+        {
+            lock (@lock)
+            {
+                instances -= 1;
+            }
+
+            if (e.Reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine(string.Concat("Active IP: ", e.Reply.Address.ToString()));
+
+                Computers comps = new Computers();
+                comps.Name = GetMachineNameFromIPAddress(e.Reply.Address.ToString());
+                comps.IPAddress = e.Reply.Address.ToString();
+                _lsActiveIPAddresses.Add(comps);
+                
+                result += 1;
+            }
+            else
+            {
+                //Console.WriteLine(String.Concat("Non-active IP: ", e.Reply.Address.ToString()))
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _backgroundWorker.RunWorkerAsync();
+
+            //Thread thread = new Thread(GetIPAddresses);
+            //thread.SetApartmentState(ApartmentState.STA);
+            //thread.IsBackground = true;
+            //thread.Start();
+            
+        }
+
+        private void btnIPAddresses_Checked(object sender, RoutedEventArgs e)
+        {
+            Height = 210;
+        }
+
+        private void btnIPAddresses_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Height = 70;
+        }
+
+        private void btnRefreshIPAddresses_Click(object sender, RoutedEventArgs e)
+        {
+            btnRefreshIPAddresses.IsEnabled = false;
+            _backgroundWorker.RunWorkerAsync();
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            GetIPAddresses();
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //grdIPAddresses.Dispatcher.Invoke((Action)(() => grdIPAddresses.ItemsSource = _lsActiveIPAddresses));
+            grdIPAddresses.ItemsSource = _lsActiveIPAddresses;
+        }
+
         #endregion
 
         #region Methods
@@ -140,7 +229,9 @@ namespace SpeedMeter
         {
             foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
             {
-                if (ni.OperationalStatus == OperationalStatus.Up && ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet && ni.NetworkInterfaceType != NetworkInterfaceType.Wireless80211)
+                if (ni.OperationalStatus == OperationalStatus.Up 
+                    && ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet 
+                    && ni.NetworkInterfaceType != NetworkInterfaceType.Wireless80211)
                 {
                     _networkInterface = ni;
                     //_interfaceStats = _networkInterface.GetIPStatistics();
@@ -185,6 +276,106 @@ namespace SpeedMeter
             }
 
             return tpl = new Tuple<string, string>(adjustedSize.ToString($"{decimalFormatString}"), sizeSuffixes[mag]);
+        }
+
+        private void GetIPAddresses()
+        {
+            result = 0;
+            _lsActiveIPAddresses.Clear();
+
+            //Get my current IP. We use this to determine the base IP on the network.
+            string baseIP = string.Empty;
+            foreach (UnicastIPAddressInformation ip in _networkInterface.GetIPProperties().UnicastAddresses)
+            {
+                if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    baseIP = $"{ip.Address.ToString().Substring(0, ip.Address.ToString().LastIndexOf("."))}.";
+                    break;
+                }
+            }
+            
+            Console.WriteLine("Pinging 255 destinations of D-class in {0}*", baseIP);
+
+            CreatePingers(255);
+
+            PingOptions po = new PingOptions(ttl, true);
+            System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+            byte[] data = enc.GetBytes("abababababababababababababababab");
+
+            SpinWait wait = new SpinWait();
+            int cnt = 1;
+
+            Stopwatch watch = Stopwatch.StartNew();
+
+            foreach (Ping p in pingers)
+            {
+                lock (@lock)
+                {
+                    instances += 1;
+                }
+
+                p.SendAsync(string.Concat(baseIP, cnt.ToString()), timeOut, data, po);
+                cnt += 1;
+            }
+
+            while (instances > 0)
+            {
+                wait.SpinOnce();
+            }
+
+            watch.Stop();
+
+            DestroyPingers();
+
+            lblFinishedTime.Dispatcher.Invoke((Action)(() =>
+                lblFinishedTime.Text = $" {watch.Elapsed.Milliseconds}ms"
+            ));
+
+            lblIPCount.Dispatcher.Invoke((Action)(() =>
+                lblIPCount.Text = $" {result}"
+            ));
+
+            btnRefreshIPAddresses.Dispatcher.Invoke((Action)(() =>
+             btnRefreshIPAddresses.IsEnabled = true
+            ));
+            //Console.ReadKey();
+        }
+
+        private void CreatePingers(int cnt)
+        {
+            for (int i = 1; i <= cnt; i++)
+            {
+                Ping p = new Ping();
+                p.PingCompleted += Ping_completed;
+                pingers.Add(p);
+            }
+        }
+
+        private void DestroyPingers()
+        {
+            foreach (Ping p in pingers)
+            {
+                p.PingCompleted -= Ping_completed;
+                p.Dispose();
+            }
+
+            pingers.Clear();
+
+        }
+
+        private string GetMachineNameFromIPAddress(string ipAdress)
+        {
+            string machineName = string.Empty;
+            try
+            {
+                IPHostEntry hostEntry = Dns.GetHostEntry(ipAdress);
+                machineName = hostEntry.HostName;
+            }
+            catch (Exception ex)
+            {
+                // Machine not found...
+            }
+            return machineName;
         }
 
         #endregion
